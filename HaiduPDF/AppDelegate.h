92039a6808b9e52994c5951b509d98b3979f454a
jaypume <i@jaypu.com>
2014-03-03 15:28:08 +0800

    Initial Commit

---
+++ HaiduPDF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HaiduPDF
+//
+//  Created by HEIZI on 14-3-3.
+//  Copyright (c) 2014年 HEIZI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
